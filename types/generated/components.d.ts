146389cef9fd231bce5f13b98aa235af8df2ef68
Andreii <49789938+andrei-manda@users.noreply.github.com>
2026-09-14 15:27:37 +0300

    chore: test (#34)

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -89,7 +89,7 @@ export interface MenuSubItem extends Struct.ComponentSchema {
 }
 
 declare module '@strapi/strapi' {
-  export namespace Public {
+  export module Public {
     export interface ComponentSchemas {
       'evaluation.dimension': EvaluationDimension;
       'evaluation.question': EvaluationQuestion;
